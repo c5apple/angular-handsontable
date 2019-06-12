689243f1ed9dcb67dc5f6df1e837f0860b6859c9
c5apple <c5apple_party@yahoo.co.jp>
2019-06-12 23:56:02 +0900

    usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-handsontable';
+
+  data: any[] = [
+    ['', 'Tesla', 'Mercedes', 'Toyota', 'Volvo'],
+    ['2019', 10, 11, 12, 13],
+    ['2020', 20, 11, 14, 13],
+    ['2021', 30, 15, 12, 13]
+  ];
 }
