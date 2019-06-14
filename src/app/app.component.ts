754ad74915dfb93779cc85e57d7173b43d18323a
c5apple <c5apple_party@yahoo.co.jp>
2019-06-15 00:47:47 +0900

    Basic Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  data: any[] = [
-    ['', 'Tesla', 'Mercedes', 'Toyota', 'Volvo'],
-    ['2019', 10, 11, 12, 13],
-    ['2020', 20, 11, 14, 13],
-    ['2021', 30, 15, 12, 13]
+  dataset: any[] = [
+    { id: 1, name: 'Ted Right', address: 'Wall Street' },
+    { id: 2, name: 'Frank Honest', address: 'Pennsylvania Avenue' },
+    { id: 3, name: 'Joan Well', address: 'Broadway' },
+    { id: 4, name: 'Gail Polite', address: 'Bourbon Street' },
+    { id: 5, name: 'Michael Fair', address: 'Lombard Street' },
+    { id: 6, name: 'Mia Fair', address: 'Rodeo Drive' },
+    { id: 7, name: 'Cora Fair', address: 'Sunset Boulevard' },
+    { id: 8, name: 'Jack Right', address: 'Michigan Avenue' },
   ];
 }
