950bc42cd32fa4edfb8bf8da03b303dd01eb8604
c5apple <c5apple_party@yahoo.co.jp>
2019-06-16 12:52:16 +0900

    Data validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,14 +23,14 @@ export class AppComponent {
 
   swapHotData() {
     this.data = [
-      { id: 1, name: 'Ted Right', address: 'Wall Street' },
-      { id: 2, name: 'Frank Honest', address: 'Pennsylvania Avenue' },
-      { id: 3, name: 'Joan Well', address: 'Broadway' },
-      { id: 4, name: 'Gail Polite', address: 'Bourbon Street' },
-      { id: 5, name: 'Michael Fair', address: 'Lombard Street' },
-      { id: 6, name: 'Mia Fair', address: 'Rodeo Drive' },
-      { id: 7, name: 'Cora Fair', address: 'Sunset Boulevard' },
-      { id: 8, name: 'Jack Right', address: 'Michigan Avenue' },
+      { id: 1, 'name.first': 'Ted', 'name.last': 'Right', ip: '0.0.0.1', email: 'Ted.Right@ex.com', address: 'Wall Street' },
+      { id: 2, 'name.first': 'Frank', 'name.last': 'Honest', ip: '0.0.0.1', email: 'Frank.Honest@ex.com', address: 'Pennsylvania Avenue' },
+      { id: 3, 'name.first': 'Joan', 'name.last': 'Well', ip: '0.0.0.1', email: 'Joan.Well@ex.com', address: 'Broadway' },
+      { id: 4, 'name.first': 'Gail', 'name.last': 'Polite', ip: '0.0.0.1', email: 'Gail.Polite@ex.com', address: 'Bourbon Street' },
+      { id: 5, 'name.first': 'Michael', 'name.last': 'Fair', ip: '0.0.0.1', email: 'Michael.Fair@ex.com', address: 'Lombard Street' },
+      { id: 6, 'name.first': 'Mia', 'name.last': 'Fair', ip: '0.0.0.1', email: 'Mia.Fair@ex.com', address: 'Rodeo Drive' },
+      { id: 7, 'name.first': 'Cora', 'name.last': 'Fair', ip: '0.0.0.1', email: 'Cora.Fair@ex.com', address: 'Sunset Boulevard' },
+      { id: 8, 'name.first': 'Jack', 'name.last': 'Right', ip: '0.0.0.1', email: 'Jack.Right@ex.com', address: 'Michigan Avenue' },
     ];
   }
 }
--- src/app/hot-table/hot-table.component.ts
+++ src/app/hot-table/hot-table.component.ts
@@ -1,6 +1,18 @@
 import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { HotTableRegisterer } from '@handsontable/angular';
 
+const ipValidatorRegexp = /^(?:\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b|null)$/;
+const emailValidator = function (value, callback) {
+  setTimeout(function () {
+    if (/.+@.+/.test(value)) {
+      callback(true);
+    }
+    else {
+      callback(false);
+    }
+  }, 1000);
+};
+
 @Component({
   selector: 'app-hot-table',
   templateUrl: './hot-table.component.html',
@@ -23,6 +35,13 @@ export class HotTableComponent implements OnInit, OnChanges {
     rowHeaders: true,
     stretchH: 'all',
     columnSorting: true,
+    columns: [
+      { data: 'id', title: 'ID', type: 'numeric' },
+      { data: 'name.first', title: 'First name' },
+      { data: 'name.last', title: 'Last name' },
+      { data: 'ip', title: 'IP', validator: ipValidatorRegexp, allowInvalid: true },
+      { data: 'email', title: 'E-mail', validator: emailValidator, allowInvalid: false }
+    ],
     contextMenu: {
       items: {
         'row_above': {
